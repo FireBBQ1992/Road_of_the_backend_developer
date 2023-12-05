ca52b2486c0236b88eec9e19e19e1a0300f77cd3
FireBBQ <firebbq1992@gmail.com>
2023-12-06 03:30:57 +0800

    更新

--- Python/Day1-15/Day7.py
+++ Python/Day1-15/Day7.py
@@ -3,7 +3,7 @@ import time
 
 
 def main():
-    content = '北京歡迎你為你開天闢地…………'
+    content = '歡迎你為你開天闢地…………'
     while True:
         # 清理螢幕上的輸出
         os.system('clear')  # os.system('cls')
@@ -11,8 +11,6 @@ def main():
         # 休眠200毫秒
         time.sleep(0.2)
         content = content[1:] + content[0]
-
-
 if __name__ == '__main__':
     main()
     main()
